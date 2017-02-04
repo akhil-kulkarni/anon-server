4f2ebec1294191fe0c5e3f37f798c130030964ed
Vitaly Tomilov <vitaly-t@users.noreply.github.com>
2017-02-04 12:46:45 +0000

    Update server.js
    the database object must be created once, and then shared ;)
    
    upgrade to the latest pg-promise, and you would see a warning about bad/repeated `db` initialization.

--- server.js
+++ server.js
@@ -8,15 +8,13 @@ var upload = multer(); // for parsing multipart/form-data
 var pgp = require("pg-promise")();
 
 var DB_URL = "postgres://localhost/anon-db";
+let db = pgp(DB_URL);
 
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 app.post('/login', function (req, res) {
 	console.log("request.body:: " + JSON.stringify(req.body));
-
-	let db = pgp(DB_URL);
-
 	db.any("SELECT * from fys_txn_userinfo where username=$1",[req.body.Key.REQ.USERNAME]).then(function (data) {
 		console.log("DATA:", JSON.stringify(data));
 		if(data.length>0) {
@@ -40,7 +38,6 @@ app.post('/register', function (req, res) {
 	console.log("in register: " + JSON.stringify(req.body));
 	let recToInsert = req.body.Key.REQ.REG_DATA;
 	if(!!recToInsert.USERNAME && !!recToInsert.PASSWORD && !!recToInsert.FIRST_NAME && !!recToInsert.LAST_NAME && !!recToInsert.EMAIL_ID && !!recToInsert.DOB && !!recToInsert.OCCUPATION && !!recToInsert.SEX && !!recToInsert.COUNTRY_CODE && !!recToInsert.STATE && !!recToInsert.CITY && !!recToInsert.ZIPCODE){
-		let db = pgp(DB_URL);
 		let parameterList = [recToInsert.USERNAME, recToInsert.PASSWORD, recToInsert.FIRST_NAME, recToInsert.MIDDLE_NAME, recToInsert.LAST_NAME, recToInsert.EMAIL_ID, recToInsert.DOB, recToInsert.OCCUPATION, recToInsert.SEX, recToInsert.COUNTRY_CODE, recToInsert.STATE, recToInsert.CITY, recToInsert.ZIPCODE];
 		db.query("insert into fys_tx_userinfo (username, password, first_name, middle_name, last_name, email_id, dob, occupation, sex, country_code, state, city, zipcode)", parameterList).then(function (data) {
 			console.log("DATA:", JSON.stringify(data));
@@ -63,9 +60,6 @@ app.post('/register', function (req, res) {
 
 app.post('/fetchData', function (req, res) {
 	console.log("fetchData request.body:: " + JSON.stringify(req.body));
-
-	let db = pgp(DB_URL);
-
 	if(!!req.body.Key.REQ && req.body.Key.REQ.ACN == "COUNTRY_LIST"){
 		db.any("select name as COUNTRY_NAME, iso as COUNTRY_CODE from fys_mst_country",[]).then(function (data) {
 			console.log("DATA:", (!!data?data.length:null));
